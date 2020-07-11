790153efa2231d94821ea36e0723d71c9d5e593f
brad toney <63473477+bravotango87@users.noreply.github.com>
2020-07-11 14:51:46 -0400

    Init

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TrashManagement.Models;
+
+namespace TrashManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashManagement.Models
+{
+    public class Customer
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+
+        public string LastName { get; set; }
+
+        [Display(Name = "Address")]
+
+        public string StreetAddress { get; set; }
+       
+
+        [Display(Name = "Zip Code")]
+        public int ZipCode { get; set; }
+
+        public double Balance { get; set; }
+
+
+
+
+        [Display(Name = "Trash Pickup Day")]
+        public string PickupDay { get; set; }
+
+        [Display(Name = "Extra Pick Day)")]
+
+        public string ExtraPickup { get; set; }
+
+
+
+
+
+        [Display(Name = "Request To Start Suspension Of Pickup")]
+        public string StartSuspend { get; set; }
+
+        [Display(Name = "Request To End Suspension Of Pickup")]
+        public string EndSuspend { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+        [ForeignKey("IdentityUser")]
+
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+
+}
